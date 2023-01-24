70b2eb69beee6475ccba962e7e99491a5aea6e6f
zuba2033 <saitman9999@gmail.com>
2023-01-24 22:51:55 +0300

    styles correction

--- src/components/welcomeBlock/WelcomeBlock.js
+++ src/components/welcomeBlock/WelcomeBlock.js
@@ -7,9 +7,10 @@ const WelcomeBlock = () => {
                 <div className="welcomeBlock__text">
                     Here you can see the photos from the surface of Mars!
                     <ul className="welcomeBlock__list">
-                        <li>1. Choose rover (the Perseverance is recommended!)</li>
-                        <li>2. Choose sol (Martian day) or click random</li>
-                        <li>3. Open slider when the gallery is loaded!</li>
+                        <li>Choose rover. The <span>Perseverance</span> is recommended!</li>
+                        <li>Choose sol (Martian day). Or click random</li>
+                        <li>Load the gallery</li>
+                        <li>Open the slider when the gallery is loaded!</li>
                     </ul>
                     <div className="welcomeBlock__contacts">
                         For any issues contact me on <a href="https://github.com/zuba2033/mars-photos/issues">GitHub!</a>
